965307c5ccf05a8840137c58e2805dffa0225525
João Guilherme <joao.sczip@hotmail.com>
2020-04-23 19:55:07 -0300

    feat: add global style

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -12,4 +12,17 @@ export default createGlobalStyle`
         background: #312E38;
         color: #FFF;
     }
+
+    body, input, button {
+        font-family: 'Roboto Slab', serif;
+        font-size: 16px;
+    }
+
+    h1, h2, h3, h4, h5, h6, strong {
+        font-weight: 500;
+    }
+
+    button {
+        cursor: pointer;
+    }
 `;
