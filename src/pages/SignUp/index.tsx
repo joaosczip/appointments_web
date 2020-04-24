b2db37e4df361875613bb7e15a2e256d86fc6733
João Guilherme <joao.sczip@hotmail.com>
2020-04-23 21:36:05 -0300

    feat: add and configure @unform

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { FiArrowLeft, FiMail, FiLock, FiUser } from "react-icons/fi";
+import { Form } from "@unform/web";
 
 import logoImg from "../../assets/logo.svg";
 import Input from "../../components/Input";
@@ -7,12 +8,16 @@ import Button from "../../components/Button";
 import { Container, Content, Background } from "./styles";
 
 const SignUp: React.FC = () => {
+  function handleSubmit(data: any) {
+    console.log(data);
+  }
+
   return (
     <Container>
       <Background />
       <Content>
         <img src={logoImg} alt="GoBarber" />
-        <form>
+        <Form onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
           <Input icon={FiUser} name="name" placeholder="Nome" />
           <Input icon={FiMail} name="email" placeholder="E-mail" />
@@ -23,7 +28,7 @@ const SignUp: React.FC = () => {
             placeholder="Senha"
           />
           <Button type="submit">Cadastrar</Button>
-        </form>
+        </Form>
         <a href="">
           <FiArrowLeft />
           Voltar para logon
