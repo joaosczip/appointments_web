0d36a31a4a0aa1b87c41109f1848a83e551fa54a
João Guilherme <joao.sczip@hotmail.com>
2020-04-28 21:05:07 -0300

    feat: add correct icon to toast

--- src/components/ToastContainer/Toast/index.tsx
+++ src/components/ToastContainer/Toast/index.tsx
@@ -1,5 +1,10 @@
-import React from "react";
-import { FiAlertCircle, FiXCircle } from "react-icons/fi";
+import React, { useEffect } from "react";
+import {
+  FiAlertCircle,
+  FiXCircle,
+  FiCheckCircle,
+  FiInfo,
+} from "react-icons/fi";
 
 import { Container } from "./styles";
 import { ToastMessage, useToast } from "../../../hooks/toast";
@@ -8,14 +13,28 @@ interface ToastProps {
   message: ToastMessage;
 }
 
+const icons = {
+  info: <FiInfo size={24} />,
+  error: <FiAlertCircle size={24} />,
+  success: <FiCheckCircle size={24} />,
+};
+
 export const Toast: React.FC<ToastProps> = ({
   message: { description, type, id, title },
 }) => {
   const { removeToast } = useToast();
 
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      removeToast(id);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, [removeToast, id]);
+
   return (
     <Container hasDescription={!!description} type={type}>
-      <FiAlertCircle />
+      {icons[type || "info"]}
       <div>
         <strong>{title}</strong>
         {description && <p>{description}</p>}
